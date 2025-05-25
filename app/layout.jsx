07dd6f305880ad56ca04fbb17f0f0ba021a34f36
Michel Marques <mitchmarques@gmail.com>
2025-05-25 18:41:30 +0200

    meta layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,22 @@ import React, { Suspense } from 'react'
 import RootClientLayout from './RootClientLayout'
 import Script from 'next/script'
 
+export const metadata = {
+  metadataBase: new URL('https://www.alforis.fr'),
+  title: {
+    default: 'Alforis – Cabinet de conseil patrimonial haut de gamme',
+    template: '%s – Alforis',
+  },
+  description: 'Découvrez notre approche patrimoniale sur mesure.',
+  openGraph: {
+    siteName: 'Alforis',
+    locale: 'fr_FR',
+    type: 'website',
+    images: ['/assets/img/og/home.png'],
+  },
+  alternates: { canonical: 'https://www.alforis.fr/' },
+}
+
 
 export default function Layout({ children }) {
 
