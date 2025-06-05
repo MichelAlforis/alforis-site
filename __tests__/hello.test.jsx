3ed39895c23f85a34999e8f0db4f493727698ed3
MichelAlforis <michel.marques@alforis.fr>
2025-06-05 13:10:11 +0200

    Add Jest testing setup

---
+++ __tests__/hello.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+function Hello() {
+  return <div>Hello World</div>
+}
+
+test('renders hello world', () => {
+  render(<Hello />)
+  expect(screen.getByText('Hello World')).toBeInTheDocument()
+})
